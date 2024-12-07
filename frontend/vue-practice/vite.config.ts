d3faddd2a84f49f57b8a544833160c8d594b990b
yamao-sys <yamaopanku@gmail.com>
2024-12-07 09:50:35 +0900

    viteからvue環境を作り直し(HMRを効かせたい)

--- frontend/vue-practice/vite.config.ts
+++ frontend/vue-practice/vite.config.ts
@@ -1,24 +1,17 @@
-import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import vueJsx from '@vitejs/plugin-vue-jsx'
-import vueDevTools from 'vite-plugin-vue-devtools'
+import path from 'path';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueJsx(),
-    vueDevTools(),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    },
-  },
+  plugins: [vue()],
   server: {
     host: '0.0.0.0',
     port: 5173,
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
   },
 })
